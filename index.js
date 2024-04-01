f983e47dd54d1de1849002c24912c60cdda8cfc2
Anirudh Goudar <anirudhgoudar@Anirudhs-MacBook-Pro.local>
2024-03-31 21:19:13 -0400

    Adding Readme

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
-import { v4 as uuidv4 } from 'uuid';
-import dotenv from 'dotenv';
-import pg from 'pg';
-const { Client } = pg;
-import { PubSub } from '@google-cloud/pubsub';
-import mailgun from 'mailgun-js';
-import { DataTypes, Sequelize } from "sequelize";
+// import { v4 as uuidv4 } from 'uuid';
+// import dotenv from 'dotenv';
+// import pg from 'pg';
+// const { Client } = pg;
+// import { PubSub } from '@google-cloud/pubsub';
+// import mailgun from 'mailgun-js';
+// import { DataTypes, Sequelize } from "sequelize";
+
+const { v4: uuidv4 } = require('uuid');
+const dotenv = require('dotenv');
+const pg = require('pg');
+const { PubSub } = require('@google-cloud/pubsub');
+const mailgun = require('mailgun-js');
+const { DataTypes, Sequelize } = require('sequelize');
+
 
 dotenv.config();
 
 
 const dbHost = process.env.DATABASE_HOST;
-const dbPort = process.env.DATABASE_PORT;
+const dbDatabase = process.env.DATABASE_NAME; 
 const dbUser = process.env.DATABASE_USER;
 const dbPassword = process.env.DATABASE_PASSWORD;
-const dbDatabase = process.env.DATABASE_NAME; 
 
-const databaseConnection = new Sequelize(dbUser,dbPassword,  dbDatabase, {
+
+console.log("dbHost ", dbHost);
+console.log("dbUser ", dbUser);
+ console.log("dbPassword ", dbPassword);
+   console.log("dbDatabase ", dbDatabase);
+
+
+const databaseConnection = new Sequelize(dbDatabase, dbUser, dbPassword, {
 host :dbHost,
 dialect :"postgres",
 
 });
+
+console.log("dbUser after", dbUser);
+ console.log("dbPassword after", dbPassword);
 
 const account = databaseConnection.define('account', {
     id: {
@@ -49,16 +66,6 @@ const account = databaseConnection.define('account', {
             isEmail: true,
         },
     },
-    account_created: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.NOW,
-    },
-    account_updated: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.NOW,
-    },
     isVerified:{
         type: DataTypes.BOOLEAN,
         allowNull: false,
@@ -72,13 +79,27 @@ const account = databaseConnection.define('account', {
         type: DataTypes.DATE,
         defaultValue: null, 
     },
-});
+    
+},
+{
+tableName: "accounts",
+    indexes: [
+      {
+        unique: true,
+        fields: ["username"],
+      },
+    ],
+    timestamps: true,
+    createdAt: "account_created",
+    updatedAt: "account_updated",
+  }
+);
 
 
 
 
 // Function to send email verification link using Mailgun API
-const sendVerificationEmail = async (email, verificationtoken) => {
+const sendVerificationEmail = async (email, verificationLink,verificationtoken) => {
     const mailgunApiKey = '325f9b0aec49fac0609d9abb27ee542c-f68a26c9-e987ec87'; // Replace with your Mailgun API key
     const mailgunDomain = 'goudar.me'; // Replace with your Mailgun domain
 
@@ -86,6 +107,8 @@ const sendVerificationEmail = async (email, verificationtoken) => {
         apiKey: mailgunApiKey,
         domain: mailgunDomain
     });
+    
+   // const verificationLink = `http://goudar.me:8080/v1/user/emailverification/${verificationtoken}`;
 
     const mailData = {
         from: 'Aniruddh Goudar <postmaster@goudar.me>',
@@ -100,15 +123,15 @@ const sendVerificationEmail = async (email, verificationtoken) => {
             }
             else{
                 console.log('Verification email sent successfully');
-                await userInfoUpdate(email, verificationtoken);
+                await module.exports.userUpdate(email, verificationtoken);
             }
         });
        
 };
 
-export const userInfoUpdate = async (email, verificationtoken) => {
+module.exports.userUpdate = async (email, verificationtoken) => {
     const expirationTime = new Date(Date.now() + 2 * 60 * 1000); // 2 minutes expiration
-  
+    console.log("expirationTime ", expirationTime);
     try {
       const user = await account.findOne({
         where: {
@@ -118,8 +141,9 @@ export const userInfoUpdate = async (email, verificationtoken) => {
       console.log("user ", user);
   
       if (user) {
-        user.verification_token = token;
+        user.verification_token = verificationtoken;
         user.expiration_time = expirationTime;
+
         await user.save();
         console.log("User verification details updated successfully.");
       } else {
@@ -130,7 +154,7 @@ export const userInfoUpdate = async (email, verificationtoken) => {
     }
   };
 
-export const handleEmailVerification = async (message, context) => {
+  module.exports.handleEmailVerification = async (message, context) => {
     try {
         const dataString = typeof message.data === 'string' ? message.data : JSON.stringify(message.data);
         console.log('Data String:', dataString);
@@ -140,10 +164,9 @@ export const handleEmailVerification = async (message, context) => {
 
         const { verificationtoken, email } = data;
 
-        const expirationTime = new Date(Date.now() + 2 * 60 * 1000); // 2 minutes expiration
-        const verificationLink = `https://goudar:8080/v1/user/emailverification/${verificationtoken}`;
+        const verificationLink = `http://goudar.me:8080/v1/user/emailverification/${verificationtoken}`;
 
-        await sendVerificationEmail(email, verificationLink);
+        await sendVerificationEmail(email, verificationLink, verificationtoken);
 
     } catch (error) {
         console.error('Error processing email verification:', error);
